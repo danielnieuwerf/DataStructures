da7ac6e111786c515d48c588f97a444ff52b68af
danielnieuwerf <60330137+danielnieuwerf@users.noreply.github.com>
2023-01-14 18:50:15 +0000

    Defined LRUCache interface

--- LRUCache/LRUCache/LRUCache.cpp
+++ LRUCache/LRUCache/LRUCache.cpp
@@ -1,20 +1,27 @@
-// LRUCache.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+
+template <typename S, typename T>
+class LRUCache {
+private:
+    uint32_t _capacity;
+
+public:
+    LRUCache(uint32_t capacity) : _capacity(capacity) {}
+
+    void put(S key, T val)
+    {
+
+    }
+
+    T get(S key)
+    {
+        return {};
+    }
+};
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    LRUCache<int, int> cache(10);
+    cache.put(1, 1);
+
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
