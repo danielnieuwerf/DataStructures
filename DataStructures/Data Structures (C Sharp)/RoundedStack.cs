1e40c5fd0aa19eacc37ea4cb9385e33e07ac1203
danielnieuwerf <60330137+danielnieuwerf@users.noreply.github.com>
2023-01-20 21:45:57 +0000

    Implemented C# Rounded Stack

--- DataStructures/Data Structures (C Sharp)/RoundedStack.cs
+++ DataStructures/Data Structures (C Sharp)/RoundedStack.cs
@@ -1,1 +1,59 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+
+namespace Data_Structures__C_Sharp_
+{
+    public class RoundedStack<T>
+    {
+        public RoundedStack(int capacity)
+        {
+            if (capacity <= 0)
+                throw new ArgumentOutOfRangeException("Capacity must exceed 0.");
+
+            this.capacity = capacity;
+            this.size = 0;
+        }
+
+        public int Count { get => size; }
+
+        // Pushes a new object to the top of the stack.
+        // If the capacity is exceeded, the bottom of the stack is dropped.
+        public void Push(T value)
+        {
+            stack.Push(value);
+            ++size;
+
+            if (size > capacity)
+            {
+                var arr = stack.ToArray();  // TODO O(n) can be improved upon.
+                stack.Clear();
+                for(int i = arr.Length - 2; i>=0; --i)
+                    stack.Push(arr[i]);
+                --size;
+            }
+        }
+
+        // Removes and returns the object at the top of the stack.
+        // Throws InvalidOperationException if stack is empty.
+        public T Pop()
+        {
+            --size;
+            return stack.Pop();
+        }
+
+        public void Clear()
+        {
+            stack.Clear();
+            size = 0;
+        }
+
+        public T[] ToArray()
+        {
+            return stack.ToArray();
+        }
+
+        private Stack<T> stack = new Stack<T>();
+        private int size = 0;
+        private int capacity = 0;
+    }
+}
